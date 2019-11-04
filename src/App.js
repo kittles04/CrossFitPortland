c86cd5c360d8ae8bbb1a6dc90523f2f8d6944a78
kerri walker <kerriannwalker3@gmail.com>
2019-11-04 14:16:33 -0800

    filter venues

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import {
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      query: ''
+    }
+  }
   componentDidMount(){
     let googleMapsPromise = load_google_maps();
     let placesPromise = load_places();
@@ -16,17 +22,18 @@ class App extends Component {
     ])
     .then(values => {
       let google = values[0];
-      let venues = values[1].response.venues;
+      this.venues = values[1].response.venues;
 
       this.google = google;
       this.markers = [];
+      this.infowindow = new google.maps.InfoWindow();
       this.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 9,
         scrollwheel: true,
-        center: { lat: venues[0].location.lat, lng: venues[0].location.lng }
+        center: { lat: this.venues[0].location.lat, lng: this.venues[0].location.lng }
       });
 
-      venues.forEach(venue => {
+      this.venues.forEach(venue => {
         let marker = new google.maps.Marker({
           position: { lat: venue.location.lat, lng: venue.location.lng },
           map: this.map,
@@ -35,25 +42,66 @@ class App extends Component {
           name: venue.name,
           animation: google.maps.Animation.DROP
         });
-      })
+
+        marker.addListener('click', () => {
+          if (marker.getAnimation() !== null) { marker.setAnimation(null); }
+				  else { marker.setAnimation(google.maps.Animation.BOUNCE); }
+				  setTimeout(() => { marker.setAnimation(null) }, 1500);
+			  });
+        google.maps.event.addListener(marker, 'click', () => {
+  			   this.infowindow.setContent(marker.name);
+				   this.map.setCenter(marker.position);
+				   this.infowindow.open(this.map, marker);
+				   this.map.panBy(0, -125);
+        });
+        
+        this.markers.push(marker);
+      });
+
+      this.setState({ filteredVenues: this.venues })
 
     })
   }
 
+  listItemClick = (venue) => {
+    let marker = this.markers.filter(m => m.id === venue.id)[0]
+    this.infowindow.setContent(marker.name);
+    this.map.setCenter(marker.position);
+    this.infowindow.open(this.map, marker);
+    this.map.panBy(0, -125);
+  }
+
+  filterVenues(query){
+    let f = this.venues.filter(venue => venue.name.toLowerCase().includes(query.toLowerCase()));
+    this.markers.forEach(marker => {
+      marker.name.toLowerCase().includes(query.toLowerCase()) == true ?
+      marker.setVisible(true) : 
+      marker.setVisible(false)
+    })
+    this.setState({ filteredVenues: f , query});
+  }
+
   render() {
   return (
-    <div id="map">
+    <div>
+      <div id="map">
 
+      </div>
+      <div id="sidebar">
+        <input  placeholder= "Filter content" value={this.state.query} onChange={(e) =>{this.filterVenues(e.target.value) }}/>
+        <br/>
+        {
+          this.state.filteredVenues && this.state.filteredVenues.length > 0 && this.state.filteredVenues.map((venue, index )=>(
+            <div key={index} className = "venue-item" onClick={()=>{ this.listItemClick(venue)}}>
+              {venue.name}
+
+            </div>
+          ))
+        }
+      </div>
     </div>
-  );
- }
+    );
+  }
 }
 
 export default App;
-// this.google = google;
-//       this.infowindow = new google.maps.InfoWindow();
-//       this.map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 9,
-//         scrollwheel: true,
-//         center: { lat: venues[0].location.lat, lng: venues[0].location.lng }
-//       });
